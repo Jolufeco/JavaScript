2bc400bd02367900260d9bb5ecc2dcc3a5f68544
Jolufeco <joaoooo.90.1@gmail.com>
2026-05-24 18:41:30 -0300

    aula 12 - ex1 ex2

---
+++ Exercícios/aula012/ex001/node.js
@@ -0,0 +1,9 @@
+var idade = 12;
+console.log(`sua idade é ${idade} anos.`)
+if (idade >=16 && idade < 18 || idade >= 67) {
+    console.log ('seu voto é opcional.')
+}  else if (idade >= 18 ) {
+    console.log('seu voto é obrigatório.')
+} else {
+    console.log ('você ainda não pode votar.')
+}
