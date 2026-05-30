98781a4e6a32c15d1b5ace9e8d4a4caf72b84404
Jolufeco <joaoooo.90.1@gmail.com>
2026-05-30 16:01:26 -0300

    Atualizações

--- desafios/desafio2/script.js
+++ desafios/desafio2/script.js
@@ -1,29 +1,44 @@
-function Verificar() {
+function verificar() {
     var data = new Date();
     var ano = data.getFullYear();
     var textnu = document.getElementById('idade')
     var texts = document.getElementsByName('bts');
     var textna = document.getElementById('Nome');
     var dados = document.getElementById('dados');
+    var nome = textna.value;
     
-    if (nome == 0 || idade >= ano ) {
+    if (nome == 0 || idade >= ano) {
         alert('Algo deu errado. Por favor ensira corretamente o nome/idade e sexo.')
         
     } else {
-        var nome = textna.value;
-        var idade = textnu.value;
+        var idade = Number(textnu.value);
         let gênero = ''
         var img = document.createElement('img');
         img.setAttribute('id', 'foto')
 
         if (texts[0].checked){
             gênero = 'Homem'
+            if (idade >= 1 && idade <= 3) {
+                img.setAttribute('src', './masculino/bebe.png')
+            } else if (idade >= 4 && idade <=12) {
+                img.setAttribute('src', './masculino/crianca.png')
+            } else if (idade >= 13 && idade <=17) {
+                img.setAttribute('src', './masculino/adolescente.png')
+            } else if (idade >= 18 && idade <=39 ) {
+                img.setAttribute('src', './masculino/Adulto.png')
+            } else if (idade >= 40) {
+                img.setAttribute('src', './masculino/idoso.png')
+            }
+
         } else {
             gênero = 'Mulher'
         }
 
+        dados.innerHTML = `detectamos  ${gênero}(${nome}) com ${idade} anos`
         dados.style.textAlign = 'center'
-        dados.innerHTML = `detectamos ${gênero} e com ${idade} `
+        dados.appendChild(img)
+        
+
     }
 
 }
