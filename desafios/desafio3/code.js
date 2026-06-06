08e823e21a1a6294c3d3acdc843e28fc34fb50e7
Jolufeco <joaoooo.90.1@gmail.com>
2026-06-06 17:52:27 -0300

    final

--- desafios/desafio3/code.js
+++ desafios/desafio3/code.js
@@ -5,21 +5,38 @@ function calculo() {
     let n3 = document.getElementById('n3')
     let local = document.getElementById('local');
 
-    if (n1.value.length == 0 || n2.value.length == 0 || n3.value.length == 0) {
-
-        window.alert('Falta dados')
+    if (n1.value.length == 0 || n2.value.length == 0) {
+        local.innerHTML = "impossível contar!"
 
     } else {
         
-        local.innerHTML = 'contando:'
+        local.innerHTML = 'contando: <br>'
 
-        let num1 = n1.value;
-        let num2 = n2.value;
-        let num3 = n3.value;
+        let num1 = Number(n1.value);
+        let num2 = Number(n2.value);
+        let num3 = Number(n3.value);
+
+        if (num3 <= 0 ) {
+            window.alert('Impossível contar com passos = 0. Será considerado = 1.')
+            num3 = 1;
+        }   
+            
+            if (num1 < num2){
+    
+                for (let c = num1; c <= num2; c+= num3) {
+                    local.innerHTML += ` <strong>${c}</strong> \u{1F449} `
+                }
+                
+            } else {
+    
+                for(let c = num1; c >= num2; c -= num3) {
+                    local.innerHTML += `<strong>${c}</strong> \u{1F449}`
+                }
+            }
+
         
-        for (let c = num1; c <= num2; c+= num3) {
-            local.innerHTML += ` ${c} `
-        }
+        local.innerHTML += `\u{1F3C1}`
+
 
     }
 
