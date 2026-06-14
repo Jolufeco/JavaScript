6871c2be23492b0d485acd8f4b1146a11f795828
Jolufeco <joaoooo.90.1@gmail.com>
2026-06-14 20:18:13 -0300

    desafio5

---
+++ Exercícios/aula015/ex001/code.js
@@ -0,0 +1,5 @@
+let num = [1, 2, 4, 9, 10]
+console.log(num)
+num.push(8)
+console.log(num)
+console.log(num.length)
---
+++ desafios/desafio5/code.js
@@ -0,0 +1,59 @@
+let number = []
+var msg = document.getElementById('msg')
+var mensa = document.createElement('p')
+
+function clicar() {
+    var n1 = document.getElementById('n1')
+    var num = n1.value
+    if (num <= 0) {
+    alert('parece que deu algum erro. por favor, digite um número.')
+} else {
+
+    var select = document.getElementById('res')
+    number.push(num)
+    
+    
+    var opcao = document.createElement('option')
+    opcao.text =`o número ${num} foi adicionado`
+    select.appendChild(opcao)
+    mensa.innerHTML = ""
+    
+
+}
+n1.value = ""
+n1.focus()
+
+    
+    
+}
+
+function finalizar() {
+    if (number == 0) {
+        alert('Sua lista não possui nenhum valor. Por favor, digite algum.')
+    } else {
+        let maior = number[0]
+        let menor = number[0]
+        var soma = 0
+        for(let pos in number) {
+           soma += number[pos]
+            if(number[pos] > maior) {
+                maior = number[pos]
+            }
+            if(number[pos] < menor) {
+                menor = number[pos]
+            }
+            
+        }
+
+        let média = soma % number.length
+
+        mensa.innerHTML = `Essa lista possui ${number.length} item(s). <br> `
+        mensa.innerHTML += `O maior número nessa lista é ${maior}. <br>`
+        mensa.innerHTML += `O menor número nessa lista é ${menor}. <br>`
+        mensa.innerHTML += `A soma desses números nessa lista é ${soma}. <br>`
+         mensa.innerHTML += `A média desses números nessa lista é ${média}. <br>`
+        msg.appendChild(mensa)  
+        
+    }
+
+}
